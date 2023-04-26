c574f1c1d5ee6f83b613cf3538fd45625835ef8c
davidetaribelli <davide.taribelli@gmail.com>
2023-04-26 15:56:19 +0200

    completed basic version

--- js/main.js
+++ js/main.js
@@ -1,19 +1,29 @@
-let gridElement = document.querySelector(".container");
+const gridElement = document.querySelector(".container");
 const btnPlay = document.querySelector(".play")
 
 btnPlay.addEventListener("click", function(){
-    for (let i = 1; i <= 100; i++){
-        gridElement.innerHTML += `<div class="square">${i}</div>`
-    }
-});
-
-
-
-
-
-
+    gridElement.innerHTML="";
+    play(100);
+}); 
 
 
 
 
 // FUNZIONI
+function play (box){
+    
+    for (let i = 0 ; i < box; i++){
+        const newSquare = document.createElement("div");
+        newSquare.classList.add("square")
+        newSquare.innerHTML=`${i+1}`
+        gridElement.appendChild(newSquare);
+
+        newSquare.addEventListener("click", function() {
+            console.log("La casella selezionata è la numero:", this.innerHTML);
+            this.classList.toggle("clicked");
+        });
+    }  
+     
+
+   
+}
